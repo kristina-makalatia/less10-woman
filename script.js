7d72bfb25f72d253dc9c1c7abc8fccc7db5778c8
kristina-makalatia <kristina.makalatia@geolab.edu.ge>
2022-10-31 18:04:02 +0400

    add responsive

---
+++ script.js
@@ -0,0 +1,33 @@
+//variables
+//data types
+//primitive values
+// let studentName = 'ani'; //string
+// let studentAge = 25; //number - float, integeri
+// let studentStatus = true; //boolean
+// let x = null; //null
+// let y; //undefined
+
+// console.log(typeof y);
+
+// let number1 = '15';
+// let number2 = 10;
+// let sum = number1 * number2;
+// console.log(sum);
+
+
+// objects
+//array - მასივი                                               
+// let studentsNames = ['ani', 'nino', 'keti', 'nini', 'levani', 'lasha' ];
+//  // index/keys>  //   0        1      2       3       4          5
+
+// console.log(studentsNames[studentsNames.length - 2]);
+
+
+//object
+let user = {
+    userName: 'nino',
+    userAge: 28,
+    userStatus: 'active'
+}
+// console.log(user.userStatus);
+// console.log(user['userName']);
